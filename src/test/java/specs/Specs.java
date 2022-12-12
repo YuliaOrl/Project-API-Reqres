542c7bde0d4ea5bb769cdc98beab591f6099ef1f
YuliaOrl <orbitalik32@gmail.com>
2022-12-12 22:54:30 +0300

    Added refactoring

--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -5,6 +5,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+
 import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static org.hamcrest.Matchers.notNullValue;
@@ -16,12 +17,6 @@ public class Specs {
             .log().uri()
             .log().body()
             .contentType(ContentType.JSON);
-
-    public static RequestSpecification requestSpec = with()
-            .filter(withCustomTemplates())
-            .baseUri("https://reqres.in")
-            .log().uri()
-            .log().body();
 
     public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
             .log(LogDetail.STATUS)
--- src/test/java/tests/ReqresInTests.java
+++ src/test/java/tests/ReqresInTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
-import java.util.concurrent.atomic.AtomicReference;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -18,183 +17,144 @@ import static specs.Specs.*;
 @Owner("yuliaorlova")
 @Feature("API тесты")
 @DisplayName("API тесты для сайта Reqres.in")
+@Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
+@Severity(SeverityLevel.CRITICAL)
 public class ReqresInTests {
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Позитивная проверка регистрации пользователя")
     void checkRegisterSuccessful() {
-        AtomicReference<RegLogResponseModel> response = new AtomicReference();
+        RegLogBodyModel requestBody = RegLogBodyModel.builder()
+                .email("eve.holt@reqres.in")
+                .password("pistol")
+                .build();
 
-        step("Регистрация пользователя", () -> {
-            RegLogBodyModel body = new RegLogBodyModel();
-            body.setEmail("eve.holt@reqres.in");
-            body.setPassword("pistol");
-
-            response.set(given()
-                .spec(requestSpecJson)
-                .body(body)
-                .when()
-                .post("/api/register")
-                .then()
-                .spec(responseSpecToken)
-                .statusCode(200)
-                .extract()
-                .as(RegLogResponseModel.class));
-        });
+        RegLogResponseModel responseModel = step("Регистрация пользователя", () ->
+                given().spec(requestSpecJson)
+                        .body(requestBody)
+                        .when()
+                        .post("/api/register")
+                        .then()
+                        .spec(responseSpecToken)
+                        .statusCode(200)
+                        .extract()
+                        .as(RegLogResponseModel.class));
 
         step("Проверка, что пользователь зарегистрирован", () -> {
-            assertThat(response.get().getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
-            assertThat(response.get().getId()).isEqualTo(4);
+            assertThat(responseModel.getToken()).isNotNull();
+            assertThat(responseModel.getId()).isNotNull();
         });
     }
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Позитивная проверка авторизации пользователя")
     void checkLoginSuccessful() {
-        AtomicReference<RegLogResponseModel> response = new AtomicReference();
+        RegLogBodyModel requestBody = RegLogBodyModel.builder()
+                .email("eve.holt@reqres.in")
+                .password("pistol")
+                .build();
 
-        step("Авторизация пользователя", () -> {
-            RegLogBodyModel body = new RegLogBodyModel();
-            body.setEmail("eve.holt@reqres.in");
-            body.setPassword("pistol");
+        RegLogResponseModel responseModel = step("Авторизация пользователя", () ->
+                given().spec(requestSpecJson)
+                        .body(requestBody)
+                        .when()
+                        .post("/api/login")
+                        .then()
+                        .spec(responseSpecToken)
+                        .statusCode(200)
+                        .extract()
+                        .as(RegLogResponseModel.class));
 
-            response.set(given()
-                .spec(requestSpecJson)
-                .body(body)
-                .when()
-                .post("/api/login")
-                .then()
-                .spec(responseSpecToken)
-                .statusCode(200)
-                .extract()
-                .as(RegLogResponseModel.class));
-        });
         step("Проверка, что пользователь авторизован", () ->
-            assertThat(response.get().getToken()).isEqualTo("QpwL5tke4Pnpja7X4"));
+                assertThat(responseModel.getToken()).isNotNull());
     }
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("normal")})
-    @Severity(SeverityLevel.NORMAL)
     @DisplayName("Проверка получения информации по пользователю")
     void checkGetSingleUserTest() {
-        AtomicReference<SingleResponseModel> response = new AtomicReference();
+        SingleResponseModel responseModel = step("Получение данных пользователя", () ->
+                given().spec(requestSpecJson)
+                        .when()
+                        .get("/api/users/2")
+                        .then()
+                        .spec(responseSpec)
+                        .statusCode(200)
+                        .extract()
+                        .as(SingleResponseModel.class));
 
-        step("Получение данных пользователя", () -> {
-            response.set(given()
-                .spec(requestSpec)
-                .when()
-                .get("/api/users/2")
-                .then()
-                .spec(responseSpec)
-                .statusCode(200)
-                .extract()
-                .as(SingleResponseModel.class));
-        });
         step("Проверка, что данные пользователя корректные", () -> {
-            assertThat(response.get().getData().getId()).isEqualTo(2);
-            assertThat(response.get().getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
-            assertThat(response.get().getData().getFirstName()).isEqualTo("Janet");
-            assertThat(response.get().getData().getLastName()).isEqualTo("Weaver");
-            assertThat(response.get().getData().getAvatar())
+            assertThat(responseModel.getData().getId()).isEqualTo(2);
+            assertThat(responseModel.getData().getEmail()).isEqualTo("janet.weaver@reqres.in");
+            assertThat(responseModel.getData().getFirstName()).isEqualTo("Janet");
+            assertThat(responseModel.getData().getLastName()).isEqualTo("Weaver");
+            assertThat(responseModel.getData().getAvatar())
                     .isEqualTo("https://reqres.in/img/faces/2-image.jpg");
-            assertThat(response.get().getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
-            assertThat(response.get().getSupport().getText())
+            assertThat(responseModel.getSupport().getUrl()).isEqualTo("https://reqres.in/#support-heading");
+            assertThat(responseModel.getSupport().getText())
                     .isEqualTo("To keep ReqRes free, contributions towards server costs are appreciated!");
         });
     }
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Тест на создание имени и работы пользователя")
     void checkPostCreateTest() {
-        AtomicReference<JobResponseModel> response = new AtomicReference();
+        JobBodyModel requestBody = JobBodyModel.builder()
+                .name("Cat")
+                .job("walk around the house")
+                .build();
 
-        step("Создание имени и работы пользователя", () -> {
-            JobBodyModel body = new JobBodyModel();
-            body.setName("Cat");
-            body.setJob("walk around the house");
+        JobResponseModel responseModel = step("Создание имени и работы пользователя", () ->
+                given().spec(requestSpecJson)
+                        .body(requestBody)
+                        .when()
+                        .post("/api/users")
+                        .then()
+                        .spec(responseSpec)
+                        .statusCode(201)
+                        .extract()
+                        .as(JobResponseModel.class));
 
-            response.set(given()
-                .spec(requestSpecJson)
-                .body(body)
-                .when()
-                .post("/api/users")
-                .then()
-                .spec(responseSpec)
-                .statusCode(201)
-                .extract()
-                .as(JobResponseModel.class));
-        });
         step("Проверка, что имя и работа пользователя созданы", () -> {
-            assertThat(response.get().getName()).isEqualTo("Cat");
-            assertThat(response.get().getJob()).isEqualTo("walk around the house");
+            assertThat(responseModel.getName()).isEqualTo("Cat");
+            assertThat(responseModel.getJob()).isEqualTo("walk around the house");
         });
     }
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
-    @DisplayName("Негативная проверка создания имени и работы пользователя")
-    void checkNegativePostCreateTest() {
-        step("Отправка некорректного запроса и проверка статуса кода в ответе", () -> {
-            given()
-                .spec(requestSpec)
-                .post("/api/users")
-                .then()
-                .spec(responseSpec)
-                .statusCode(415);
-        });
-    }
-
-    @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Тест на редактирование имени и работы пользователя")
     void checkPutUpdateTest() {
-        AtomicReference<JobResponseModel> response = new AtomicReference();
+        JobBodyModel requestBody = JobBodyModel.builder()
+                .name("Kitty")
+                .job("sleep all day")
+                .build();
 
-        step("Редактирование имени и работы пользователя", () -> {
-            JobBodyModel body = new JobBodyModel();
-            body.setName("Kitty");
-            body.setJob("sleep all day");
+        JobResponseModel responseModel = step("Редактирование имени и работы пользователя", () ->
+                given().spec(requestSpecJson)
+                        .body(requestBody)
+                        .when()
+                        .put("/api/users/2")
+                        .then()
+                        .spec(responseSpec)
+                        .statusCode(200)
+                        .extract()
+                        .as(JobResponseModel.class));
 
-            response.set(given()
-                .spec(requestSpecJson)
-                .body(body)
-                .when()
-                .put("/api/users/2")
-                .then()
-                .spec(responseSpec)
-                .statusCode(200)
-                .extract()
-                .as(JobResponseModel.class));
-        });
         step("Проверка, что имя и работа пользователя отредактированы", () -> {
-            assertThat(response.get().getName()).isEqualTo("Kitty");
-            assertThat(response.get().getJob()).isEqualTo("sleep all day");
+            assertThat(responseModel.getName()).isEqualTo("Kitty");
+            assertThat(responseModel.getJob()).isEqualTo("sleep all day");
         });
     }
 
     @Test
-    @Tags({@Tag("api"), @Tag("high"), @Tag("critical")})
-    @Severity(SeverityLevel.CRITICAL)
     @DisplayName("Тест на удаление пользователя")
     void checkDeleteTest() {
-        step("Отправка запроса на удаление пользователя и проверка статуса кода в ответе", () -> {
-            given()
-                .spec(requestSpec)
-                .when()
-                .delete("/api/users/2")
-                .then()
-                .spec(responseSpec)
-                .statusCode(204);
-        });
+        step("Отправка запроса на удаление пользователя и проверка статуса кода в ответе", () ->
+                given().spec(requestSpecJson)
+                        .when()
+                        .delete("/api/users/2")
+                        .then()
+                        .spec(responseSpec)
+                        .statusCode(204));
     }
 }
 
